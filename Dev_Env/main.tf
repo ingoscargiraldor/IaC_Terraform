1ccb1e19bcc527dd75d893c49655a6a3a2e7888b
ingoscargiraldor <ing.oscargiraldor@gmail.com>
2024-05-06 10:50:07 -0500

    Prueba S3

--- Dev_Env/main.tf
+++ Dev_Env/main.tf
@@ -1,1 +1,7 @@
-
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "mi-bucket-unico-probe-2" # Nombre único para tu bucket
+  tags = {
+    Name        = "Mi Bucket"
+    Environment = "Producción"
+  }
+}
