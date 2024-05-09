5310c34d8f8be7b6cb2a0670f0aa987331b91e3b
ingoscargiraldor <ing.oscargiraldor@gmail.com>
2024-05-09 15:18:20 -0500

    prueba lock state id

--- Dev_Env/backend.tf
+++ Dev_Env/backend.tf
@@ -4,8 +4,6 @@ terraform {
     key    = "path/Dev-Env.tfstate"
     region = "us-east-1"
 
-    # Enable during Step-09     
-    # For State Locking
     dynamodb_table = "test-tf-iac-880616236833"  
   }
 }
