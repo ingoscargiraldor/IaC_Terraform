b079b5ce4bc60a2083ce66162db70af675ba7671
ingoscargiraldor <ing.oscargiraldor@gmail.com>
2024-05-10 16:48:56 -0500

    prueba lock state id

--- Dev_Env/main.tf
+++ Dev_Env/main.tf
@@ -1,11 +1,3 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mi-bucket-unico-probe-2" # Nombre único para tu bucket
-  tags = {
-    Name        = "Mi Bucket"
-    Environment = "Producción"
-  }
-}
-
 resource "aws_s3_bucket" "my_bucket_2" {
   bucket = "mi-bucket-unico-probe-24" # Nombre único para tu bucket
   tags = {
